438c00a79815a281a2ccb52b5065eb6eb7279ab9
kristinatrajkovic <63244948+kristinatrajkovic@users.noreply.github.com>
2023-01-29 23:10:56 +0100

    Add files via upload

---
+++ Projekat PIA/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Progetto</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <span id="page-title">Progetto</span>
+                <div id="login-btn">
+                    <button class="btn">Log in</button>
+                    <button class="btn">Sign up</button>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <section>
+                <img src="" alt="">
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, repellendus fugiat dolor impedit dolorum quam a in omnis est sequi unde. Architecto vel, dolore minus asperiores, repellendus enim, repudiandae totam dignissimos adipisci voluptas quas placeat. Nulla animi exercitationem perspiciatis beatae?</div>
+            </section>
+            <section>
+                <div>
+                    <h1></h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facere voluptatibus eum sed, soluta inventore dignissimos, perferendis quam labore deleniti architecto nostrum voluptates fugit quisquam eligendi ducimus beatae illo a?</p>
+                </div>
+                <div>
+                    <h1></h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facere voluptatibus eum sed, soluta inventore dignissimos, perferendis quam labore deleniti architecto nostrum voluptates fugit quisquam eligendi ducimus beatae illo a?</p>
+                </div>
+                <div>
+                    <h1></h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facere voluptatibus eum sed, soluta inventore dignissimos, perferendis quam labore deleniti architecto nostrum voluptates fugit quisquam eligendi ducimus beatae illo a?</p>
+                </div>
+            </section>
+            <section>
+                <h3></h3>
+                <div>
+                    <p></p>
+                    <img src="" alt="">
+                </div>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
